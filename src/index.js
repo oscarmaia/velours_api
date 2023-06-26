a9d502bece49b1df606184d2a41ce0fc2f034e18
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 19:00:04 -0300

    feat: add routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/health.routes.js";
+import healthRouter from "./routes/health.routes.js";
+import productRouter from "./routes/product.routes.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use(productRouter);
+app.use(healthRouter);
+
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => console.log(`Running at port ${PORT}`));
