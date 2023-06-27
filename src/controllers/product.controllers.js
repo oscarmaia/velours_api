5aefd2b88076a0e0184327595a580360b3b5b3d4
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 22:01:52 -0300

    feat: create controller

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,17 +1,59 @@
-import productRepository from "../repositories/product.repositories.js";
-
-export async function getAllProducts() {
-  try {
-  } catch (error) {}
-}
+import productService from "../service/product.service.js";
 
 export async function addProduct(req, res) {
   try {
     const { name, discount, price, image, quantity } = req.body;
-    await productRepository.addProduct(name, discount, price, image, quantity);
-    res.send("adicionado no mongo").status(201);
+    await productService.addProduct(name, discount, price, image, quantity);
+    res.sendStatus(201);
   } catch (error) {
+    console.log(error);
     res.sendStatus(500);
-    console.log("falhou no addProduct");
+  }
+}
+
+export async function getProducts(req, res) {
+  try {
+    const result = await productService.getProducts();
+    if (result) {
+      res.send(result).status(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function updateProducts(req, res) {
+  const { id } = req.params;
+  const data = req.body;
+  console.log(id);
+  try {
+    const result = await productService.updateProducts(id, data);
+    if (result) {
+      res.send(result).status(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteProduct(req, res) {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const result = await productService.deleteProduct(id);
+    if (result?.deletedCount === 1) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
   }
 }
