2cd3594342f67b12bd94103970961cc3a2b8dad2
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 22:03:13 -0300

    feat: product CRUD

--- src/repositories/product.repositories.js
+++ src/repositories/product.repositories.js
@@ -1,18 +1,52 @@
-import { productsCollectioon } from "../database/mongo.js";
-import 
-function addProduct(name, discount, price, image, quantity) {
-  productsCollectioon.insertOne({
-    _id: "testeIds2",
-    name,
-    discount: parseFloat(discount),
-    price: parseFloat(price),
-    image,
-    quantity,
-  });
+import { ObjectId } from "mongodb";
+import { productsCollection } from "../database/mongo.js";
+
+async function addProduct(name, discount, price, image, quantity) {
+  try {
+    await productsCollection.insertOne({
+      name,
+      discount: parseInt(discount),
+      price: parseInt(price),
+      image,
+      quantity,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getProducts() {
+  try {
+    return productsCollection.find({}).toArray();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateProducts(id, data) {
+  try {
+    return productsCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: data }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteProduct(id, data) {
+  try {
+    return productsCollection.deleteOne({ _id: new ObjectId(id) });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const productRepository = {
   addProduct,
+  getProducts,
+  updateProducts,
+  deleteProduct,
 };
 
 export default productRepository;
